c081606b1731ab54eba0d1fbf7d67f3cc81b2edb
KumoHakase//KumoTech <87934760+kumohakasemk9@users.noreply.github.com>
2023-04-30 14:24:37 +0900

    Update main.cpp
    Added copyright section

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+ /*
+	STM32F4XX arm-none-eabi-g++ Project Template (ROM load)
+	Main routine
+	(C) 2023 Kumohakase
+	You are free to destribute, modify without modifying this section.
+	Please consider support me on kofi.com https://ko-fi.com/kumohakase
+*/
+
 #include <stm32f4xx.h>
 
 int main() {
